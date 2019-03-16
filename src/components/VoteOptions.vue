b86f21235bfea57a94e699e2ce77c85f2133476f
Marek Mitis <mmitis@grupa.onet>
2019-03-16 12:54:02 +0100

    [#3.1] Part 3

---
+++ src/components/VoteOptions.vue
@@ -0,0 +1,36 @@
+<template>
+  <button
+    type="button"
+    v-bind="$attrs"
+    :disabled="loading"
+    :class="classes"
+  >
+    <slot/>
+  </button>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    loading: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    disabled: function () {
+      return this.loading || this.$attrs.disabled;
+    },
+    classes: function () {
+      return `btn-primary ${this.loading ? 'btn-loading' : ''}`;
+    }
+  }
+
+};
+</script>
+<style>
+  .btn-loading {
+    border: 1px solid red;
+  }
+</style>
